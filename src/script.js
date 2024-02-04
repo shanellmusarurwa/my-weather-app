b4048b24dec0be3073a4d644086bb5b9b65d513e
shanellmusarurwa <155754152+shanellmusarurwa@users.noreply.github.com>
2024-02-04 14:14:28 +0200

    updated my js by adding a search engine

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  console.log(searchInput);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
