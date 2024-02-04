8ffbd04c84e24addae0d58dd0a5ae4968bbebb48
shanellmusarurwa <155754152+shanellmusarurwa@users.noreply.github.com>
2024-02-04 14:13:49 +0200

    upgrading my html

--- src/script.js
+++ src/script.js
@@ -1,2 +1,7 @@
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput);
+}
