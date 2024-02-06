1812690f1d29f0e998ed8b50fb877d9b508acab2
shanellmusarurwa <155754152+shanellmusarurwa@users.noreply.github.com>
2024-02-06 20:40:50 +0200

    Replace dummy content with real time data

--- src/script.js
+++ src/script.js
@@ -52,29 +52,45 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Frid", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function displayForecast() {
-  let days = ["Tue", "Wed", "Thur", "Frid", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml = forecastHtml`  <div class="weather-forecast-day">
+  response.data.d <
+    aily.forEach(function (day, index) {
+      if (index < 5) {
+        forecastHtml = forecastHtml`  <div class="weather-forecast-day">
             
-                <div class="weather-forecast-date">${day}</div>
-                <div class="weather-forecast-icon">⛅</div>
+                <div class="weather-forecast-date">${formatDay(day.time)}</div>
+                 
+                <img src ="${
+                  day.condition.icon_url
+                }"class="weather-forecast-icon" >
+                
                   <div class="weather-forecast-temperatures>
                 <div class="weather-forecast-temperature">
-                  <strong>15°</strong> 
+                  <strong>${Math.round(day.temperature.maximum)}°</strong> 
                 </div>
-                <div class="weather-forecast-temperature">9°</div>
+                <div class="weather-forecast-temperature">${Math.round(
+                  day.temperature.minimum
+                )}°</div>
             </div>
                   </div>
                 
               
             </div>
           </div>`;
-  });
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
+      }
+
+      let forecastElement = document.querySelector("#forecast");
+      forecastElement.innerHTML = forecastHtml;
+    });
 }
 
 let searchFormElement = document.querySelector("#search-form");
