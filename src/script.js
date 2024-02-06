4ff2f8e62a981b4b8ab7233cd8841d4d6b08bc66
shanellmusarurwa <155754152+shanellmusarurwa@users.noreply.github.com>
2024-02-06 16:30:46 +0200

    Added the forecast dummy data

--- src/script.js
+++ src/script.js
@@ -51,5 +51,32 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Frid", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml = forecastHtml`  <div class="weather-forecast-day">
+            
+                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-icon">⛅</div>
+                  <div class="weather-forecast-temperatures>
+                <div class="weather-forecast-temperature">
+                  <strong>15°</strong> 
+                </div>
+                <div class="weather-forecast-temperature">9°</div>
+            </div>
+                  </div>
+                
+              
+            </div>
+          </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+console.log(displayForecast);
